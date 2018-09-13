0ef62653ad2f6532e31fe061cb03d435a7892b47
Ivan Mitronov <imitronov@gmail.com>
2018-09-13 17:36:59 +0300

    add iconpacks

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,51 @@
+/* Feather */
+var iconPack = 'feather';
+var iconPrefix = 'fe';
+
+/* ZondIcons */
+// var iconPack = 'zondicons';
+// var iconPrefix = 'zi';
+
+/* 60 Vicons */
+// var iconPack = '60vicons';
+// var iconPrefix = 'vi';
+
+/* LinearIcons Free */
+// var iconPack = 'linearicons';
+// var iconPrefix = 'lnr';
+
+/* Entypo + */
+// var iconPack = 'entypo';
+// var iconPrefix = 'et';
+
+/*******************************************/
+
+var src	= './';
+
 var gulp 			= require("gulp"),
-	sass				= require("gulp-sass"),
-	csso				= require("gulp-csso"),
+	sass			= require("gulp-sass"),
+	csso			= require("gulp-csso"),
 	autoprefixer	= require("gulp-autoprefixer"),
 	rename			= require("gulp-rename"),
 	concat			= require("gulp-concat"),
 	font2css		= require("gulp-font2css").default,
 	iconfont		= require("gulp-iconfont"),
-	iconfontCss	= require("gulp-iconfont-css")
-	gulpSequence = require('gulp-sequence');
+	iconfontCss		= require("gulp-iconfont-css")
+	gulpSequence	= require('gulp-sequence');
 
-var src	= './';
+/*******************************************/
 
 gulp.task( "font", function() {
-	return gulp.src([src + "node_modules/feather-icons/dist/icons/*.svg"])
+	return gulp.src([src + "icons/" + iconPack + "/*.svg"])
 		.pipe(iconfontCss({
-			fontName: "fe",
-			cssClass: "fe",
+			fontName: iconPrefix,
+			cssClass: iconPrefix,
 			path: src + "src/template.scss",
-			targetPath: "../dist/feather.scss",
+			targetPath: "../dist/" + iconPack + ".scss",
 			fontPath: src + "dist/"
 		}))
 		.pipe(iconfont({
-			fontName: "fe",
+			fontName: iconPrefix,
 			normalize: true,
 			fontHeight: 1000
 		}))
@@ -29,7 +53,7 @@ gulp.task( "font", function() {
 });
 
 gulp.task( "sass", function() {
-	return gulp.src([ src + "dist/feather.scss" ])
+	return gulp.src([ src + "dist/" + iconPack + ".scss" ])
 		.pipe( rename( { suffix: ".min", prefix : ""} ) )
 		.pipe( sass() )
 		.pipe( gulp.dest( src + "dist/" ) );
@@ -38,13 +62,13 @@ gulp.task( "sass", function() {
 gulp.task( "base64", function() {
 	return gulp.src( src + "dist/*.{woff, ttf}" )
 		.pipe( font2css() )
-		.pipe( concat("font.min.css") )
+		.pipe( concat( iconPrefix + "-font.min.css") )
 		.pipe( gulp.dest( src + "dist/" ) );
 } );
 
 gulp.task( "css", function() {
-	return gulp.src([ src + "dist/feather.min.css", src + "dist/font.min.css" ])
-		.pipe( concat( "fe-icons.min.css" ) )
+	return gulp.src([ src + "dist/" + iconPack + ".min.css", src + "dist/" + iconPrefix + "-font.min.css" ])
+		.pipe( concat( iconPrefix + "-icons.min.css" ) )
 		.pipe( csso() )
 		.pipe( autoprefixer() )
 		.pipe( gulp.dest( src + "dist/css/" ) );
